fdf5f46d4710888e6fa9d1d1dc7ce4608779280f
pritam-sasmal04 <ps657700@gmail.com>
2024-09-05 21:54:27 +0530

    add contact us page

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const app = express();
+const nodemailer = require("nodemailer");
+const cookieparser = require("cookie-parser");
 const path = require("path");
 const multer = require("multer");
+const session = require("express-session");
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
 app.use(express.static("css"))
 app.use(express.static("js"))
 app.use(express.static("images"))
@@ -13,30 +18,26 @@ app.use(express.static("public/files"))
 app.use(express.static("public/images"))
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded());
-app.use(express.static("css"));
-const cookieparser = require("cookie-parser");
-const session = require("express-session");
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("css"));
+app.use(cookieparser());
+app.use(session({
+    saveUninitialized: true,
+    resave: false,
+    secret: 'sdjvbh@wdbk',
+    cookie: { maxAge: 1000 * 24 * 60 * 60 }
+}));
+app.use('*.css', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/css');
+    next();
+});
 mongoose.connect("mongodb://127.0.0.1:27017/gropoto").then((server) => {
     console.log("database connected");
 }).catch((err) => {
     console.log("error", err);
 })
 const Users = require("./models/userModel");
-const oneday = 1000 * 24 * 60 * 60;
-app.use(cookieparser());
-app.use(session({
-    saveUninitialized: true,
-    resave: false,
-    secret: 'sdjvbh@wdbk',
-    cookie: { maxAge: oneday }
-}));
-app.use('*.css', (req, res, next) => {
-    res.setHeader('Content-Type', 'text/css');
-    next();
-});
 app.get("/logout", (req, res) => {
-    //console.log("logout");
     req.session.destroy();
     res.redirect("/");
 })
@@ -105,6 +106,41 @@ function profileauth(req, res, next) {
         res.status(201).send("please login");
     }
 }
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, './user/contact.html'));
+})
+app.post('/submit-contact', (req, res) => {
+    const { name, email, subject, message } = req.body;
+    // Set up the nodemailer transporter
+    console.log(process.env.email+" "+email);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.email, 
+            pass: process.env.password, 
+        },
+        secure: true,
+    });
+
+    // Set up the email options
+    const mailOptions = {
+        from: email,
+        to: process.env.email,
+        subject: `Contact Form Submission: ${subject}`,
+        text: `You have received a new message from ${name} (${email}):\n\n${message}`,
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.send(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('Your message has been sent successfully!');
+        }
+    });
+});
+
 app.get("/signup", (req, res) => {
     res.render("signup", { message: "" });
 })
@@ -122,18 +158,18 @@ const upload = multer({ storage: storage });
 
 
 app.post("/signup", upload.single('image'), async (req, res) => {
-    const { name, email, password, address, role} = req.body;
-    const imagePath = req.file ? req.file.path : null; 
+    const { name, email, password, address, role } = req.body;
+    const imagePath = req.file ? req.file.path : null;
     //console.log(req.body.image);
     const hashedPassword = await bcrypt.hash(password, 10);
     const existUser = await Users.findOne({ email: email });
     if (existUser) {
         return res.render("signup", { message: "This email is already in use" });
     }
-    if(role==="admin"){
+    if (role === "admin") {
         return res.render("signup", { message: "you are not admin" });
     }
-    const userObj = new Users({ name, email, password: hashedPassword, address, role, image:`${email}.jpg` });
+    const userObj = new Users({ name, email, password: hashedPassword, address, role, image: `${email}.jpg` });
     userObj.save().then(response => {
         req.session.name = req.body.name;
         req.session.email = req.body.email;
@@ -155,7 +191,7 @@ app.post("/login", async (req, res) => {
             return res.render("login", { message: "Invalid email or password" });
         }
 
-        
+
         bcrypt.compare(req.body.password, response.password, (err, result) => {
             if (err || !result) {
                 return res.render("login", { message: "Invalid email or password" });
